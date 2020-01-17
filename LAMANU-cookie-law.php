860bbfddd670917f81290d4ee8b2c9e561475ddf
sylviekutz <sylviekutz@gmail.com>
2020-01-17 19:59:40 +0100

    correction chemin

--- LAMANU-cookie-law.php
+++ LAMANU-cookie-law.php
@@ -1,27 +1,28 @@
 <?php
-/**
- * Plugin Name: LAMANU-cookie-law
- * Description: WordPress Plugin for european cookie law
- * Version: 1.0
- */
-add_action('admin_menu','google_analytics_setup_menu');
-function google_analytics_setup_menu(){
-      add_menu_page('Google analytics', 'Google analytics', 'manage_options', 'config-cookie', 'analytics_page' );
+/*
+Plugin Name: LAMANU-cookie-law
+Version: 0.1
+Description: WordPress Plugin for european cookie law.
+Author: Tonyio
+*/
+// wp_register_script : fonction qui permet de référencer un script
+
+function LAMANU_scripts(){
+    wp_enqueue_script('tarteaucitron', plugin_dir_url(__FILE__) . 'js/tarteaucitron/tarteaucitron.js', [], '1.3', true);
+    wp_enqueue_script('tarteaucitron-init', plugin_dir_url(__FILE__) . 'js/tarteaucitron-init.js', [], '1.0', true);
+    wp_enqueue_script('services-cookies', plugin_dir_url(__FILE__) . 'js/services.js', [], '1.0', true);
 }
-//fonction pour importer le fichier option
-function analytics_page(){
-    require_once plugin_dir_path (__FILE__) . 'view/option.php';
+add_action('wp_enqueue_scripts', 'LAMANU_scripts');
+
+function menu_LAMANU_cookie(){
+    add_menu_page('LAMANU cookie page', 'LAMANU cookie law', 'manage_options', 'config-cookie', 'init_menu_cookie');
 }
-function LAMANU_scripts() {
-    wp_enqueue_script( 'tarteaucitron', plugin_dir_url(__FILE__) . 'js/tarteaucitron/tarteaucitron.js', [], '1.3', true);
-    wp_enqueue_script( 'tarteaucitron-init', plugin_dir_url(__FILE__) . 'js/tac_init.js', [], '1.0', true);
-    wp_enqueue_script( 'servicedescookies', plugin_dir_url(__FILE__) . 'js/service.js', [], '1.0', true);
-    wp_enqueue_script( 'google-analytics', plugin_dir_url(__FILE__) . 'js/googleAnalytics.js', [], '1.0', true);
+add_action('admin_menu', 'menu_LAMANU_cookie');
+
+function init_menu_cookie(){
+    require_once plugin_dir_path(__FILE__) . 'view/option.php';
 }
-add_action( 'wp_enqueue_scripts', 'LAMANU_scripts' );
-// function qui ajoute des enregistre des paramètres (nom du option group, nom de l'input)
 function register_settings(){
-    register_setting('LAMANU_settings_cookie', 'googleId');
+    register_setting('LAMANU_settings', 'googleID');
 }
-//initialisation de la function 
-add_action( 'admin_init', 'register_settings');
+add_action('admin_init', 'register_settings');
