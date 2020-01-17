5d0543ee964b54dbafcd3a15413080a3a4ea0217
sylviekutz <sylviekutz@gmail.com>
2020-01-17 15:10:59 +0100

    correction wordpress

---
+++ js/services.js
@@ -0,0 +1,3 @@
+tarteaucitron.user.analyticsUa = "<?php echo get_option('googleId', 'UA-XXXXXXX-XX'); ?>";
+tarteaucitron.user.analyticsMore = function () { /* add here your optionnal ga.push() */ };
+(tarteaucitron.job = tarteaucitron.job || []).push('analytics');
